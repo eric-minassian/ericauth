6bb98d6ddd859e3cb61658b9fe47111d6a36dbd7
Eric Minassian <minassianeric2848@gmail.com>
2025-01-18 15:59:58 -0800

    chore: local server deployment

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use std::env::set_var;
-
 use ericauth::{routes::routes, state::initialize_state};
-use lambda_http::{run, tracing, Error};
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    tracing::init_default_subscriber();
-
-    set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
-
+async fn main() {
     let app = routes().with_state(initialize_state());
 
-    run(app).await
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+// use lambda_http::{run, tracing, Error};
+// use std::env::set_var;
+
+// #[tokio::main]
+// async fn main() -> Result<(), Error> {
+//     tracing::init_default_subscriber();
+
+//     set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
+
+//     let app = routes().with_state(initialize_state());
+
+//     run(app).await
+// }

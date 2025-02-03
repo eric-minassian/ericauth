ef33cf96c273967ddd88953096768b75cbab0a98
Eric Minassian <minassianeric2848@gmail.com>
2025-02-03 14:13:25 -0800

    refactor: remove unused modules and new login bin

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,14 @@
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { Stack, StackProps } from "aws-cdk-lib";
 import {
   Certificate,
   CertificateValidation,
 } from "aws-cdk-lib/aws-certificatemanager";
-import { AttributeType, TableV2 } from "aws-cdk-lib/aws-dynamodb";
 import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
 import { ApiGateway } from "aws-cdk-lib/aws-route53-targets";
-import { RustFunction } from "cargo-lambda-cdk";
 import { Construct } from "constructs";
-
-import path = require("path");
+import { Api } from "./constructs/api";
+import { Database } from "./constructs/database";
+import { Lambdas } from "./constructs/lambda";
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -27,45 +25,20 @@ export class CdkStack extends Stack {
       validation: CertificateValidation.fromDns(hostedZone),
     });
 
-    const usersTable = new TableV2(this, "UsersTable", {
-      tableName: "UsersTable",
-      partitionKey: { name: "email", type: AttributeType.STRING },
-      removalPolicy: RemovalPolicy.RETAIN,
+    const database = new Database(this, "Database");
+    const lambdas = new Lambdas(this, "Lambdas", database.usersTable);
+    const api = new Api(this, "ApiGateway", {
+      domainName: DOMAIN_NAME,
+      certificate,
+      healthHandler: lambdas.healthHandler,
+      signupHandler: lambdas.signupHandler,
+      loginHandler: lambdas.loginHandler,
     });
-
-    const healthHandler = new RustFunction(this, "HealthFunction", {
-      manifestPath: path.join(__dirname, "..", ".."),
-      binaryName: "health",
-    });
-
-    const loginHandler = new RustFunction(this, "LoginFunction", {
-      manifestPath: path.join(__dirname, "..", ".."),
-      binaryName: "login",
-    });
-
-    usersTable.grantReadWriteData(loginHandler);
-
-    const api = new RestApi(this, "Api", {
-      restApiName: "eric-auth",
-      domainName: {
-        domainName: DOMAIN_NAME,
-        certificate,
-      },
-    });
-
-    const health = api.root.addResource("health");
-    health.addMethod("GET", new LambdaIntegration(healthHandler));
-
-    // const users = api.root.addResource("user");
-    // users.addMethod("POST");
-
-    const login = api.root.addResource("login");
-    login.addMethod("POST", new LambdaIntegration(loginHandler));
 
     new ARecord(this, "ARecord", {
       zone: hostedZone,
       recordName: DOMAIN_NAME,
-      target: RecordTarget.fromAlias(new ApiGateway(api)),
+      target: RecordTarget.fromAlias(new ApiGateway(api.api)),
     });
   }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,48 @@
-pub mod config;
-pub mod db;
-pub mod routes;
-pub mod state;
+use aws_sdk_dynamodb::{types::AttributeValue, Client};
+use lambda_http::Error;
+
+const USER_TABLE_NAME: &str = "UsersTable";
 
 pub struct User {
     pub email: String,
     pub password_hash: String,
 }
 
-pub const USER_TABLE_NAME: &str = "UsersTable";
+pub async fn get_user(client: &Client, email: String) -> Result<Option<User>, Error> {
+    let res = client
+        .get_item()
+        .table_name(USER_TABLE_NAME)
+        .key("email", AttributeValue::S(email.to_string()))
+        .send()
+        .await?;
+
+    match res.item {
+        Some(item) => {
+            if let Some(AttributeValue::S(password_hash)) = item.get("password_hash") {
+                Ok(Some(User {
+                    email: email.to_string(),
+                    password_hash: password_hash.clone(),
+                }))
+            } else {
+                Err(Error::from("missing password_hash"))
+            }
+        }
+        None => Ok(None),
+    }
+}
+
+pub async fn insert_user(
+    client: &Client,
+    email: String,
+    password_hash: String,
+) -> Result<(), Error> {
+    client
+        .put_item()
+        .table_name(USER_TABLE_NAME)
+        .item("email", AttributeValue::S(email))
+        .item("password_hash", AttributeValue::S(password_hash))
+        .send()
+        .await?;
+
+    Ok(())
+}
